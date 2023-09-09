ab38713915343faac57afdc97bcf64a4d1328fa7
Crowdbotics <team@crowdbotics.com>
2023-09-09 06:38:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "macrombi-43296",
+  url: "https://macrombi-43296.botics.co",
+  api: "https://macrombi-43296.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
